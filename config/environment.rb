223071401a5cc900767f36a2ff5cd894190d5958
李剑 <12120850@163.com>
2015-12-24 14:28:34 +0800

    门店修改

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Initialize the Rails application.
-Rails.application.initialize!
+
 
 module ProductConfig
   DYNAMIC_FIELDS = Hash.new
@@ -13,4 +12,9 @@ module RestConfig
 
   #PRODUCT_SERVER = 'http://localhost:3001/'
   PRODUCT_SERVER = ENV["PHOTO_HOST"] || 'http://10.99.99.206:3001/'
+
+  COUSTOMER_SERVER = 'http://10.99.99.206:82/'
 end
+
+# Initialize the Rails application.
+Rails.application.initialize!

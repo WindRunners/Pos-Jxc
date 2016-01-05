d55582ba39b5c586ae878525e1a59ec0ca8ab95d
耿科 <18638293566@163.com>
2016-01-05 13:55:34 +0800

    完善移动登录

--- config/routes.rb
+++ config/routes.rb
@@ -152,6 +152,8 @@ Rails.application.routes.draw do
     get "users/sessions/cheak_mobile",to: "users/sessions#cheak_mobile"
     get "users/send_message",to: "users/registrations#send_message"
     post "users/sessions/fix_password",to: "users/sessions#fix_password"
+    post "users/mobile_sign_in", to: "users/mobile_sessions#create"
+    post "users/mobile_sign_out", to: "users/mobile_sessions#destroy"
   end
 
 

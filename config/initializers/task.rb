294436ab7150614b4fd5b7a34383d827f69588fc
liushifeng <277827580@qq.com>
2016-01-06 10:49:13 +0800

    no message

--- config/initializers/task.rb
+++ config/initializers/task.rb
@@ -48,129 +48,129 @@ scheduler = Rufus::Scheduler.new
 #end
 
 #每隔一分钟检查活动状态
-scheduler.cron '*/1 * * * *' do
-   @full_reductions = FullReduction.where(:end_time => {"$gte" => Time.now})
-   @full_reductions += Coupon.where(:end_time => {"$gte" => Time.now}, :aasm_state => {"$not" => {"$eq" => "invalid"}})
-   @full_reductions += PromotionDiscount.where(:end_time => {"$gte" => Time.now})
-   @full_reductions.each do |fr|
-     today = Time.now.strftime('%Y%m%d%H%M%S').to_i
-     startTime = fr.start_time.strftime('%Y%m%d%H%M%S').to_i
-     endTime = fr.end_time.strftime('%Y%m%d%H%M%S').to_i
-     puts "today===//#{today}//===startTime==//#{startTime}//==endTime=//#{endTime}//"
-     if today >= startTime && today < endTime
-       puts "start======"
-       fr.save if fr.start if "beging" != fr.aasm_state
-     elsif today < startTime
-       puts "ready======"
-       fr.save if fr.ready if "noBeging" != fr.aasm_state
-     else
-       puts "stop======="
-       fr.save if fr.stop if "end" != fr.aasm_state
-     end
-   end
-end
-
-
-#从OA获取闪屏广告
-scheduler.cron '*/60 * * * *' do
-#scheduler.cron '40 03 * * *' do
-  url = RestConfig::OA_SERVER + 'api/v1/ads/splash_screen?type=JYD'
-
-
-  headers = {}
-  headers['X-Warehouse-Rest-Api-Key'] = '5604a417c3666e0b7300001a'
-
-
-  response = RestClient.get(url, headers)
-
-
-  json = JSON.parse(response.body)
-
-  begin
-    splash = Splash.find(json['id'])
-    splash.destroy
-  rescue
-  end
-
-  avatar = RestConfig::OA_SERVER + json.delete('avatar')
-
-  splash = Splash.new(json)
-
-  splash.avatar = avatar
-
-  if splash.save
-    Rails.logger.info "#{splash.id} has been saved."
-    {success: "#{splash.id} has been saved."}
-  else
-    Rails.logger.info splash.errors
-    error!({"message" => splash.errors}, 202)
-  end
-
-  Rails.logger.info "自动抓取闪屏广告成功"
-end
-
-#从OA获取轮播图
-
-scheduler.cron '*/60 * * * *' do
-#scheduler.cron '40 03 * * *' do
-
-  url = RestConfig::OA_SERVER + 'api/v1/ads/carousels?type=JYD'
-
-
-  headers = {}
-  headers['X-Warehouse-Rest-Api-Key'] = '5604a417c3666e0b7300001a'
-
-
-  response = RestClient.get(url, headers)
-
-
-  array = JSON.parse(response.body)
-
-  array.each do |json|
-
-    begin
-      carousel = Carousel.find(json['id'])
-      carousel.destroy
-    rescue
-    end
-
-    avatar = RestConfig::OA_SERVER + json.delete('avatar')
-
-    carousel = Carousel.new(json)
-
-    carousel.avatar = avatar
-
-    if carousel.save
-      Rails.logger.info "#{carousel.id} has been saved."
-      {success: "#{carousel.id} has been saved."}
-    else
-      Rails.logger.info carousel.errors
-      error!({"message" => carousel.errors}, 202)
-    end
-  end
-
-
-  Rails.logger.info "自动抓取轮播图成功"
-end
-
-#小B取现
-scheduler.at Time.now do
-  # params = {
-  #     partner_trade_no: '123123123',
-  #     openid: 'oM39dwfGmGhYZg4p7GNooywQx6B8',
-  #     check_name: 'FORCE_CHECK',
-  #     re_user_name: '耿科',
-  #     amount: 1,
-  #     desc: '提现',
-  #     spbill_create_ip: '127.0.0.1'
-  # }
-  #
-  # r = WxPay::Service.invoke_transfer(params)
-  #
-  # Rails.logger.info r
-  #
-  Rails.logger.info "取现成功"
-end
+# scheduler.cron '*/1 * * * *' do
+#    @full_reductions = FullReduction.where(:end_time => {"$gte" => Time.now})
+#    @full_reductions += Coupon.where(:end_time => {"$gte" => Time.now}, :aasm_state => {"$not" => {"$eq" => "invalid"}})
+#    @full_reductions += PromotionDiscount.where(:end_time => {"$gte" => Time.now})
+#    @full_reductions.each do |fr|
+#      today = Time.now.strftime('%Y%m%d%H%M%S').to_i
+#      startTime = fr.start_time.strftime('%Y%m%d%H%M%S').to_i
+#      endTime = fr.end_time.strftime('%Y%m%d%H%M%S').to_i
+#      puts "today===//#{today}//===startTime==//#{startTime}//==endTime=//#{endTime}//"
+#      if today >= startTime && today < endTime
+#        puts "start======"
+#        fr.save if fr.start if "beging" != fr.aasm_state
+#      elsif today < startTime
+#        puts "ready======"
+#        fr.save if fr.ready if "noBeging" != fr.aasm_state
+#      else
+#        puts "stop======="
+#        fr.save if fr.stop if "end" != fr.aasm_state
+#      end
+#    end
+# end
+#
+#
+# #从OA获取闪屏广告
+# scheduler.cron '*/60 * * * *' do
+# #scheduler.cron '40 03 * * *' do
+#   url = RestConfig::OA_SERVER + 'api/v1/ads/splash_screen?type=JYD'
+#
+#
+#   headers = {}
+#   headers['X-Warehouse-Rest-Api-Key'] = '5604a417c3666e0b7300001a'
+#
+#
+#   response = RestClient.get(url, headers)
+#
+#
+#   json = JSON.parse(response.body)
+#
+#   begin
+#     splash = Splash.find(json['id'])
+#     splash.destroy
+#   rescue
+#   end
+#
+#   avatar = RestConfig::OA_SERVER + json.delete('avatar')
+#
+#   splash = Splash.new(json)
+#
+#   splash.avatar = avatar
+#
+#   if splash.save
+#     Rails.logger.info "#{splash.id} has been saved."
+#     {success: "#{splash.id} has been saved."}
+#   else
+#     Rails.logger.info splash.errors
+#     error!({"message" => splash.errors}, 202)
+#   end
+#
+#   Rails.logger.info "自动抓取闪屏广告成功"
+# end
+#
+# #从OA获取轮播图
+#
+# scheduler.cron '*/60 * * * *' do
+# #scheduler.cron '40 03 * * *' do
+#
+#   url = RestConfig::OA_SERVER + 'api/v1/ads/carousels?type=JYD'
+#
+#
+#   headers = {}
+#   headers['X-Warehouse-Rest-Api-Key'] = '5604a417c3666e0b7300001a'
+#
+#
+#   response = RestClient.get(url, headers)
+#
+#
+#   array = JSON.parse(response.body)
+#
+#   array.each do |json|
+#
+#     begin
+#       carousel = Carousel.find(json['id'])
+#       carousel.destroy
+#     rescue
+#     end
+#
+#     avatar = RestConfig::OA_SERVER + json.delete('avatar')
+#
+#     carousel = Carousel.new(json)
+#
+#     carousel.avatar = avatar
+#
+#     if carousel.save
+#       Rails.logger.info "#{carousel.id} has been saved."
+#       {success: "#{carousel.id} has been saved."}
+#     else
+#       Rails.logger.info carousel.errors
+#       error!({"message" => carousel.errors}, 202)
+#     end
+#   end
+#
+#
+#   Rails.logger.info "自动抓取轮播图成功"
+# end
+#
+# #小B取现
+# scheduler.at Time.now do
+#   # params = {
+#   #     partner_trade_no: '123123123',
+#   #     openid: 'oM39dwfGmGhYZg4p7GNooywQx6B8',
+#   #     check_name: 'FORCE_CHECK',
+#   #     re_user_name: '耿科',
+#   #     amount: 1,
+#   #     desc: '提现',
+#   #     spbill_create_ip: '127.0.0.1'
+#   # }
+#   #
+#   # r = WxPay::Service.invoke_transfer(params)
+#   #
+#   # Rails.logger.info r
+#   #
+#   Rails.logger.info "取现成功"
+# end
 
 
 scheduler.at Time.now do

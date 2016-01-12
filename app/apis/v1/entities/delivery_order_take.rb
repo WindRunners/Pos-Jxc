e6725830d19a6cc0c4213dc73b4aca6891451a6c
flydrago <1162040314@qq.com>
2016-01-12 13:20:38 +0800

    商品详细页面修改及配送端数据暴漏去除null值

--- app/apis/v1/entities/delivery_order_take.rb
+++ app/apis/v1/entities/delivery_order_take.rb
@@ -7,7 +7,9 @@ module Entities
     expose :distance, documentation: {type: Float, desc: '配送距离'}
     expose :store_address, documentation: {type: String, desc: '门店地址'}
     expose :workflow_state, documentation: {type: String, desc: '状态 generation:待付款,paid:待抢单,take:待接货,distribution:配送中,receive:配送完成,completed:确认收货,cancelled:取消订单'}
-    expose :remarks, documentation: {type: String, desc: '重要说明'}
+    expose :remarks, documentation: {type: String, desc: '重要说明'}do |instance, options|
+      "#{instance.remarks}"
+    end
 
     expose :store_address, documentation: {type: String, desc: '门店地址'}
   end

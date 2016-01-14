cc69922e61ed7df09dc8039827b981da993ea051
flydrago <1162040314@qq.com>
2016-01-14 17:31:18 +0800

    分享日志添加积分记录

--- app/models/share_integral_record.rb
+++ app/models/share_integral_record.rb
@@ -10,4 +10,20 @@ class ShareIntegralRecord
   belongs_to :share_integral
 
 
+
+  after_save do
+
+    if self.is_confirm == 1 #登录回执
+
+      share_integral = self.share_integral
+
+      #分享者积分记录
+      share_interalLog = IntegralLog.new({'customer_id' => self.shared_customer_id, 'quantity' => share_integral.shared_give_integral, 'memo' => '推荐有礼赠送积分'})
+      share_interalLog.save
+
+      #注册者积分记录
+      register_interalLog = IntegralLog.new({'customer_id' => self.register_customer_id, 'quantity' => share_integral.register_give_integral, 'memo' => '新用户注册赠送积分'})
+      register_interalLog.save
+    end
+  end
 end

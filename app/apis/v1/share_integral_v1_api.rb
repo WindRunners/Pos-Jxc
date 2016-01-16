afe3983b956b05a49092fd5c248bb1d9af552eee
flydrago <1162040314@qq.com>
2016-01-16 13:57:19 +0800

    分享积分更新

--- app/apis/v1/share_integral_v1_api.rb
+++ app/apis/v1/share_integral_v1_api.rb
@@ -14,7 +14,7 @@ class ShareIntegralV1API < Grape::API
     now_date = Time.now
     share_integral = ShareIntegral.where({'start_date' => {'$lte' => now_date}, 'end_date' => {'$gte' => now_date}, 'status' => 1}).first()
     if share_integral.present?
-      share_integral['share_url'] = "/share_integrals/share"
+      share_integral['share_url'] = "/share_integrals/share?shared_customer_id=#{params[:customer_id]}"
       present share_integral, with: Entities::ShareIntegral
     else
       share_integral = ShareIntegral.new()

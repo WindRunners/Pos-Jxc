f16953d80cac5a99c1d0136b5debb74983c8d9c8
李剑 <12120850@163.com>
2016-01-19 11:24:37 +0800

    接口修改

--- app/apis/v1/entities/announcement.rb
+++ app/apis/v1/entities/announcement.rb
@@ -7,7 +7,12 @@ module Entities
     expose :sequence, documentation: {type: Integer, desc: '排序字段'}
     expose :source, documentation: {type: String, desc: '来源'}
     expose :pic_path, documentation: {type: Array, desc: '列表图片路径数组'} do |announcement, options|
-      announcement.pic_path = announcement.pic_thumb_path if announcement.pic_thumb_path.present? && !announcement.pic_thumb_path.empty?
+      if announcement.pic_thumb_path.present? && !announcement.pic_thumb_path.empty?
+        announcement.pic_path = announcement.pic_thumb_path
+      else
+        announcement.pic_path = announcement.pic_path
+
+      end
     end
     expose :announcement_category_id, documentation: {type: String, desc: '分类ID'}
     expose :created_at, documentation: {type: String, desc: '创建时间'}

ed7e1a026d7232f0d90d58d00cb4da8601d337ce
李剑 <12120850@163.com>
2016-01-22 14:33:13 +0800

    酒券接口时间格式化

--- app/apis/v1/entities/card_bag_list.rb
+++ app/apis/v1/entities/card_bag_list.rb
@@ -4,8 +4,12 @@ module Entities
     expose :title, documentation: {type: String, desc: '酒券标题'}
     expose :logo, documentation: {type: String, desc: '酒券logo_url'}
     expose :register_customer_count, documentation: {type: Integer, desc: '酒券领取需要达到的注册用户数'}
-    expose :start_date, documentation: {type: DateTime, desc: '酒券开始时间'}
-    expose :end_date, documentation: {type: DateTime, desc: '酒券结束时间'}
+    expose :start_date, documentation: {type: DateTime, desc: '酒券开始时间'} do |product_ticket, options|
+      product_ticket.start_date = product_ticket.start_date.strftime("%Y-%m-%d %H:%M:%S") if !product_ticket.start_date.nil?
+    end
+    expose :end_date, documentation: {type: DateTime, desc: '酒券结束时间'}do |product_ticket, options|
+      product_ticket.end_date = product_ticket.end_date.strftime("%Y-%m-%d %H:%M:%S") if !product_ticket.end_date.nil?
+  end
     expose :status, documentation: {type: String, Integer: '小C卡包内酒券当前状态#0,未失效，1，邀请成功，-1，已失效'}
     expose :product_id, documentation: {type: String, desc: '酒券附赠商品ID'}, safe: true
     expose :product_title, documentation: {type: String, desc: '酒券附赠商品标题'}, safe: true
--- app/apis/v1/entities/product_ticket.rb
+++ app/apis/v1/entities/product_ticket.rb
@@ -2,8 +2,12 @@ module Entities
   class ProductTicket < Grape::Entity
     expose :id, documentation: {type: String, desc: '酒券id'}
     expose :title, documentation: {type: String, desc: '标题'}
-    expose :start_date, documentation: {type: DateTime, desc: '开始时间'}
-    expose :end_date, documentation: {type: DateTime, desc: '结束时间'}
+    expose :start_date, documentation: {type: DateTime, desc: '酒券开始时间'} do |product_ticket, options|
+      product_ticket.start_date = product_ticket.start_date.strftime("%Y-%m-%d %H:%M:%S") if !product_ticket.start_date.nil?
+    end
+    expose :end_date, documentation: {type: DateTime, desc: '酒券结束时间'}do |product_ticket, options|
+      product_ticket.end_date = product_ticket.end_date.strftime("%Y-%m-%d %H:%M:%S") if !product_ticket.end_date.nil?
+    end
     expose :desc, documentation: {type: String, desc: '分享描述'}
     expose :rule_content, documentation: {type: String, desc: '活动规则'}
     expose :register_customer_count, documentation: {type: Integer, desc: '达到用户注册量'}

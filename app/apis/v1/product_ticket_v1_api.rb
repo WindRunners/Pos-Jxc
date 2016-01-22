e564d7ee1f00ea2f02f910c9cd02b528abef0e85
李剑 <12120850@163.com>
2016-01-22 13:47:47 +0800

    foxbug

--- app/apis/v1/product_ticket_v1_api.rb
+++ app/apis/v1/product_ticket_v1_api.rb
@@ -9,10 +9,9 @@ class ProductTicketV1API < Grape::API
     requires :status, type: Integer, desc: '酒券状态,0未邀请,1邀请成功,-1已失效'
   end
   get 'card_bag_list' do
-    case params[:status]
-      when params[:status]==0
+      if params[:status]==0
         card_bags = CardBag.where(:customer_id => params[:customer_id],:status=>0,:start_date=>{"$lte" => Time.now},:end_date=>{"$gte" => Time.now})
-      when params[:status]==1
+      elsif params[:status]==1
         card_bags = CardBag.where(:customer_id => params[:customer_id],:status=>1)
       else
         card_bags = CardBag.where(:customer_id => params[:customer_id],:status=>0,:end_date=>{"$lt" => Time.now})

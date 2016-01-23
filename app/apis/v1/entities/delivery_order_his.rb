90f490710c4ef0923829569bab5358f716f3f023
flydrago <1162040314@qq.com>
2016-01-23 14:50:22 +0800

    配送端订单暴露订单号和时间

--- app/apis/v1/entities/delivery_order_his.rb
+++ app/apis/v1/entities/delivery_order_his.rb
@@ -1,7 +1,9 @@
 module Entities
 
   class DeliveryOrderHis < Grape::Entity
+    format_with(:iso_timestamp) { |dt| dt.strftime("%Y-%m-%d %H:%M:%S") if dt.present? }
     expose :id, documentation: {type: String, desc: '订单id'}
+    expose :orderno, documentation: {type: String, desc: '订单号'}
     expose :address, documentation: {type: String, desc: '收货人地址'}
     expose :distance, documentation: {type: Float, desc: '配送距离'}do |instance, options|
       instance.distance.present? ?  (instance.distance/1000).round(1) : 0
@@ -12,5 +14,8 @@ module Entities
     end
 
     expose :store_address, documentation: {type: String, desc: '门店地址'}
+    with_options(format_with: :iso_timestamp) do
+      expose :created_at, documentation: {type: DateTime, desc: '更新时间'}
+    end
   end
 end

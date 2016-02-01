bee80954df49fc46c70823eeb03d0a77ccedb4c8
耿科 <18638293566@163.com>
2016-02-01 18:37:53 +0800

    完善测试数据

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -260,9 +260,9 @@ class ProductsController < ApplicationController
         @product = Product.with(collection: current_user.shop_id).find(p['id'])
       rescue
         @product = Product.new(p)
-        @product.price = Faker::Commerce.price
+        @product.price = Faker::Commerce.price + 1
         @product.purchasePrice = @product.price * 0.8
-        @product.stock = Faker::Number.between(1, 10)
+        @product.stock = Faker::Number.between(1, 10000)
         @product.integral = Faker::Number.between(1, 10)
         @product.state = State.find_by(name: "已上架")
 

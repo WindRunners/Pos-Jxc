95496ec5a04561e5cf5b16d23636ec2bba4508a0
flydrago <1162040314@qq.com>
2016-02-15 16:42:12 +0800

    用户管理更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,7 +48,7 @@ class User
   field :audit_desc, type: String #审批备注信息
 
   field :channel_id #移动设备推送ID
-  field :user_flag, type: Integer #用户标识  0为普通用户，1：初始化用户（不可修改和删除）
+  field :user_flag, type: Integer, default: 0 #用户标识  0为普通用户，1：初始化用户（不可修改和删除）
 
   #field :admin
 

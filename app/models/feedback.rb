06b0d031b77660402f94ac8d2c2b206c5e34361a
宋星磊 <1176898959@qq.com>
2016-02-16 16:51:22 +0800

    用户反馈 验证优化提交

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -6,7 +6,7 @@ class Feedback
   field :real_name, type: String
   field :feedback_content, type: String
 
-  validates :real_name, presence: true,length: { maximum: 20, too_long: "名称最大长度为%{count}个字符" }
+  validates :real_name, presence: true,length: { maximum: 20, too_long: "最大长度为%{count}个字符" }
   validates :mobile, presence: true, format: {with: /\A\d{11}\z/, message: "不合法!"}
   validates :feedback_content, presence: true
   belongs_to :delivery_user
